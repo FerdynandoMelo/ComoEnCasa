6e36f85bdf69ff3bb28358d80fe81c3492693407
Ferdynando <ferd.melo@duocuc.cl>
2025-11-12 07:34:43 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ComoEnCasa',
+  webDir: 'www'
+};
+
+export default config;
